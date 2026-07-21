b1a7deac9e05c51e53fb4161ce67a08613481bc4
unknown <farismmk@gmail.com>
2026-07-20 18:15:25 -0700

    use zig-build-helpers

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-A Beef wrapper for [Amoeba](https://github.com/starwing/amoeba), 
-a Cassowary constraint solving algorithm implemented in pure C.
+# About
 
-# Running the example
-
-Clone the repo, and open up the workspace in the Beef IDE, and run.
+A Beef wrapper for [Amoeba](https://github.com/starwing/amoeba), a Cassowary constraint solving algorithm implemented in pure C.
 
 # Building static libs
 
 ```sh
 zig build --prefix libs
 ```
+
+# Running the example
+
+Clone the repo, and open up the workspace in the Beef IDE, and run.
+
+# View the upstream commit against latest
+
+https://github.com/[google/brotli](https://github.com/starwing/amoeba)/compare/c79cd288e515366dc1bf70339dc9db03cca12459..master
--- build.zig
+++ build.zig
@@ -1,72 +1,18 @@
 const std = @import("std");
-
-const optimize_matrix: []const std.builtin.OptimizeMode = &.{
-    .Debug,
-    .ReleaseSmall,
-};
-
-const targets_matrix: []const std.Target.Query = &.{
-    .{ .cpu_arch = .x86_64, .os_tag = .windows, .abi = .msvc },
-    .{ .cpu_arch = .aarch64, .os_tag = .windows, .abi = .gnu },
-
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
-    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
-
-    .{ .cpu_arch = .x86_64, .os_tag = .macos },
-    .{ .cpu_arch = .aarch64, .os_tag = .macos },
-};
-
-const upstream_name = "upstream";
-
-const lib_name = "amoeba";
-
-const lib_name_debug = "amoeba_d";
-
-const sources = &.{"src/amoeba.c"};
+const zbh = @import("zbh");
 
 pub fn build(b: *std.Build) !void {
-    const target: []const u8 = b.option([]const u8, "target", "specify target triple") orelse "";
+    const upstream = b.dependency("upstream", .{});
+    const target = b.option([]const u8, "target", "");
 
-    const upstream = b.dependency(upstream_name, .{});
-
-    for (optimize_matrix) |o| {
-        for (targets_matrix) |t| {
-            const triple = t.zigTriple(b.allocator) catch "";
-
-            const mod = b.createModule(.{
-                .target = b.resolveTargetQuery(t),
-                .optimize = o,
-                .sanitize_c = .off,
-                .stack_check = false,
-                .stack_protector = false,
-                .single_threaded = true,
-            });
-
-            mod.linkSystemLibrary("c", .{});
-
-            // mod.linkSystemLibrary("ucrt");
-
-            mod.addIncludePath(upstream.path(""));
-
-            mod.addCSourceFiles(.{ .files = sources });
-
-            const lib = b.addLibrary(.{
-                .name = if (o == .Debug) lib_name_debug else lib_name,
-                .linkage = .static,
-                .root_module = mod,
-            });
-
-            const target_output = b.addInstallArtifact(lib, .{
-                .dest_dir = .{
-                    .override = .{
-                        .custom = triple,
-                    },
-                },
-            });
-
-            if (std.mem.eql(u8, target, "") or std.mem.eql(u8, target, triple)) {
-                b.getInstallStep().dependOn(&target_output.step);
-            }
-        }
-    }
+    _ = try zbh.lib(b, .{
+        .name = "amoeba",
+        .target = target,
+        .includes = &.{
+            upstream.path(""),
+        },
+        .files = .{
+            .files = &.{"src/amoeba.c"},
+        },
+    });
 }
